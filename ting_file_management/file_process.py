2a659216b356c32ec090c07df9b407f12fbf3a5b
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-13 18:01:14 -0300

    Feat: Implementa verificação de posição invalida

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,4 +1,5 @@
 from ting_file_management.file_management import txt_importer
+import sys
 
 processed = set()
 
@@ -37,4 +38,6 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position < 0 or position >= len(instance):
+        print("Posição inválida", file=sys.stderr)
+        return
