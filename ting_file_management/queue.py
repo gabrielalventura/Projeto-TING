aefe4556c0aee9c4fdb49b97fbfbb61d0a4b9d4c
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-13 13:54:07 -0300

    Feat: Adiciona funções de init, len, enqueue e dequeue na class Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,22 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.size()
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self._data) == 0:
+            return None
+        return self._data.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
+
+
+# funções baseadas no repositório da aula 3.4 e
+# no material do course do dia 3.4
