7916febaea9d77ce58a2c091d400feaea7ab157e
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-13 15:22:02 -0300

    Feat: Implementa função de importação de textos em arquivos no formato .txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -5,6 +5,13 @@ def txt_importer(path_file):
     if not path_file.endswith('.txt'):
         print('Formato inválido', file=sys.stderr)
         return
+    try:
+        with open(path_file, 'r') as file:
+            txt = file.read().split('\n')
+            return txt
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+        return
 
 # referencia endswith:
 # https://www.w3schools.com/python/ref_string_endswith.asp
