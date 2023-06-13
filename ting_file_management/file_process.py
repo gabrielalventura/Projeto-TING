e17bcd409093666f807b4bcb7756b8f29de97b77
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-13 16:23:12 -0300

    Feat: Implementa função Process, criando o dicionário

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,25 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    processed = set()
+
+    if path_file in processed:
+        return
+
+    txt = txt_importer(path_file)
+    dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt),
+        "linhas_do_arquivo": txt,
+    }
+
+    instance.enqueue(dict)  # adiciona dict na fila
+
+    processed.add(path_file)
+    # adiciona o arquivo ao conjunto de arquivos processados
+
+    print(dict)
 
 
 def remove(instance):
