9ba1852928cb664b06044e10942f42bd267171bd
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-13 15:16:01 -0300

    Feat: Implementa verificação de formato de arquivo

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print('Formato inválido', file=sys.stderr)
+        return
+
+# referencia endswith:
+# https://www.w3schools.com/python/ref_string_endswith.asp
+# e consulta material seção 01 do módulo de Ciências da Computação
