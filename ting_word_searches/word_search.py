e30f1ee548c96f68fcfa91177c0d909b1592f1c4
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-14 14:35:29 -0300

    Feat: Implementa função que verifica se a palavra existe na fila

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    existents = []
+    word_lower = word.lower()
+    for i in range(len(instance)):
+        info = instance.search(i)
+        word_info = {
+            "palavra": word,
+            "arquivo": info["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for i, line in enumerate(info["linhas_do_arquivo"], start=1):
+            line_lower = line.lower()
+            if word_lower in line_lower:
+                return word_info["ocorrencias"].append({"linha": i})
+        if word_info["ocorrencias"]:
+            existents.append(word_info)
+    return existents
 
 
 def search_by_word(word, instance):
