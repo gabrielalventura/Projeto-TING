40e292faa278287856f55fefdf10e11efe7fa14a
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-06-14 14:42:42 -0300

    Add: Adiciona referencias ao uso de função enumerate em python

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -17,6 +17,11 @@ def exists_word(word, instance):
             existents.append(word_info)
     return existents
 
+# referencia uso enumerate:
+# https://pythonhelp.wordpress.com/2012/01/05/a-funcao-enumerate/
+# https://www.hashtagtreinamentos.com/enumerate-no-python?gad=1&gclid=CjwKCAjwyqWkBhBMEiwAp2yUFmRuBprKLIuwrbA_SJf56Cpv0poNoLM3w1IrdmMlvRIpr4vsoiwClxoCWiwQAvD_BwE
+# https://datascience.eu/pt/programacao/enumerar-em-python-o-que-voce-precisa-saber/
+# https://www.w3schools.com/python/ref_func_enumerate.asp
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
